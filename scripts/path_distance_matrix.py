32e54cf407be754ee9c79d8c47e4efe7d67255b4
Erik Mohn <erikmoh@stud.ntnu.no>
2023-01-20 00:23:56 +0100

    improved osm efficiency

--- scripts/path_distance_matrix.py
+++ scripts/path_distance_matrix.py
@@ -7,13 +7,23 @@ import networkx as nx
 from pyrosm import OSM
 from tqdm.auto import tqdm
 from multiprocessing import Process, Pipe
-from coordinate_converter import node_to_id, get_route_info
+from coordinate_converter import node_to_id, calc_dist
 
+
+PLOT_GRAPH = False
+
+PROCESSES = 16
+
+FILENAME_OLD = None
+FILENAME_NEW = "od_paths"
+
+ox.settings.use_cache = True
+UPDATE_GRAPH = False
+UPDATE_CLOSEST_GRIDS = False
 
 UPDATE_LOCATION_PERIOD = 5
 STRICT_SIMPLIFYING = False
 REMOVE_RINGS = True
-PROCESSES = 16
 HIGHWAY_SPEEDS = {
     'motorway': 80,
     'trunk': 70,
@@ -26,6 +36,9 @@ HIGHWAY_SPEEDS = {
 
 
 def init():
+    if not UPDATE_GRAPH:
+        return ox.load_graphml('data/osm/simplified.graphml', edge_dtypes={"oneway": str})
+
     # initialize the reader
     osm = OSM("data/osm/oslo_akershus_highways.osm.pbf")
     # read nodes and edges of the 'driving' network
@@ -73,8 +86,57 @@ def load_coordinates():
     return combined, ids, xs, ys, grids
 
 
-def shortest_paths(G, ids, from_ids, ssb_ids, nearest_nodes, grids, conn):
-    print(f"Finding shortest path from id {from_ids[0]}-{from_ids[-1]} in process", getpid())
+def get_closest_grids(G, grids):
+    if not UPDATE_CLOSEST_GRIDS:
+        with open(f'data/nodes_closest_grids.json', 'r') as r:
+            return json.load(r)
+
+    closest_grids = {}
+    for node in tqdm(G.nodes):
+        closest = None
+        minDist = math.inf
+        # get node coordinates
+        node_x = G.nodes[node]['x']
+        node_y = G.nodes[node]['y']
+        # loop all grids to find closest
+        for coordinate in grids.itertuples():
+            grid_id, grid_y, grid_x, _ = coordinate
+            # calc distances
+            dist = calc_dist(grid_x, grid_y, node_x, node_y)
+            if dist < minDist:
+                minDist = dist
+                closest = grid_id
+        # save grid id
+        closest_grids[node] = closest
+
+    # saves to file
+    with open(f'data/nodes_closest_grids.json', 'w') as f:
+        json.dump(closest_grids, f, indent=2)
+    return closest_grids
+
+
+def get_route_info(G, ssb_ids, route, update_period, closest_grids):
+    travel_time = 0
+    section_time = 0
+    route_grids = []
+    # iterate edges in route
+    for u, v in zip(route[:-1], route[1:]):
+        edge_time = G.edges[(u, v, 0)]['travel_time']
+        travel_time += edge_time
+        section_time += edge_time
+        # save position every x min along route
+        if section_time > update_period*60:
+            section_time = 0
+            closest = closest_grids[u]
+            if closest:
+                route_grids.append(ssb_ids[closest])
+    
+    travel_time = round(travel_time)
+    return travel_time, route_grids
+
+
+def shortest_paths(G, ids, from_ids, ssb_ids, nearest_nodes, closest_grids, conn):
+    print(f"Finding shortest paths from id {from_ids[0]}-{from_ids[-1]} in process", getpid())
     od = {}
     for id1 in tqdm(from_ids):
         ssb_id1 = ssb_ids[id1]
@@ -102,10 +164,10 @@ def shortest_paths(G, ids, from_ids, ssb_ids, nearest_nodes, grids, conn):
             target_node = nearest_nodes[id2]
 
             # find shortest path (by travel time)
-            route = nx.shortest_path(G, source_node, target_node, weight="travel_time")
+            route = nx.shortest_path(G, source_node, target_node, weight="travel_time", method="bellman-ford")
 
             # get travel time and route grids
-            travel_time, route_grids = get_route_info(G, grids, ssb_ids, route, UPDATE_LOCATION_PERIOD)
+            travel_time, route_grids = get_route_info(G, ssb_ids, route, UPDATE_LOCATION_PERIOD, closest_grids)
 
             # remove destination from route if it was added
             if ssb_id2 in route_grids:
@@ -120,17 +182,28 @@ def main():
     print("Initializing reader and loading graph...")
     G = init()
 
-    print("Assigning speed and travel times for edges...")
-    G = assign_travel_time(G)
+    if UPDATE_GRAPH:
+        print("Assigning speed and travel times for edges...")
+        G = assign_travel_time(G)
 
-    print("Simplifying graph...")
-    G = simplify_graph(G)
+        print("Simplifying graph...")
+        G = simplify_graph(G)
+
+        print("Saving graph...")
+        ox.save_graphml(G, 'data/osm/simplified.graphml')
+
+    if PLOT_GRAPH:
+        print("Printing graph...")
+        ox.plot_graph(G)
 
     print("Getting coordinates from csv...")
     combined, ids, xs, ys, grids = load_coordinates()
 
     print("Finding the coordinates' closest nodes...")
     nearest_nodes = ox.nearest_nodes(G, xs, ys, False)
+
+    print("Finding the nodes' closest grids...")
+    closest_grids = get_closest_grids(G, grids)
 
     print("Creating ssb ids from coordinates...")
     ssb_ids = [node_to_id(id, type, xs, ys) for id, _, _, type in combined.itertuples()]
@@ -142,7 +215,7 @@ def main():
     for split in range(PROCESSES):
         conn1, conn2 = Pipe()
         split_ids = ids[split_size*split:split_size*(split+1)]
-        processes.append(Process(target=shortest_paths, args=(G, ids, split_ids, ssb_ids, nearest_nodes, grids, conn2)))
+        processes.append(Process(target=shortest_paths, args=(G, ids, split_ids, ssb_ids, nearest_nodes, closest_grids, conn2)))
         connections.append(conn1)
 
     print("Getting shortest paths from and to all coordinates...")
@@ -150,11 +223,11 @@ def main():
         process.start()
 
     od = {}
-    """ 
-    print("Loading od path matrix")
-    with open(f'data/od_paths', 'r') as r:
-        od = json.load(r)
-    """
+    if FILENAME_OLD:
+        print("Loading od path matrix")
+        with open(f'data/{FILENAME_OLD}', 'r') as r:
+            od = json.load(r)
+
     print("Upserting new paths from processes to od matrix")
     for connection in connections:
         od1 = connection.recv()
@@ -172,7 +245,7 @@ def main():
         process.join()
         
     print("Saving od to file...")
-    with open(f'data/od_paths.json', 'w') as f:
+    with open(f'data/{FILENAME_NEW}.json', 'w') as f:
         json.dump(od, f, indent=2)
     print("Done.")
 
