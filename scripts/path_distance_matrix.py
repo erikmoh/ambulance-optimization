e26bb85d8a8fd799113916b356d7501ee3d9a425
Erik Mohn <erikmoh@stud.ntnu.no>
2023-01-26 19:34:43 +0100

    Improved efficiency and realism of travel time

--- scripts/path_distance_matrix.py
+++ scripts/path_distance_matrix.py
@@ -10,9 +10,11 @@ from multiprocessing import Process, Pipe
 from coordinate_converter import node_to_id, calc_dist
 
 
+UPDATE_PERIOD = 5 # minutes
+
 PLOT_GRAPH = False
 
-PROCESSES = 16
+PROCESSES = 15
 
 FILENAME_OLD = None
 FILENAME_NEW = "od_paths"
@@ -21,23 +23,27 @@ ox.settings.use_cache = True
 UPDATE_GRAPH = False
 UPDATE_CLOSEST_GRIDS = False
 
-UPDATE_LOCATION_PERIOD = 5
 STRICT_SIMPLIFYING = False
 REMOVE_RINGS = True
 HIGHWAY_SPEEDS = {
-    'motorway': 80,
-    'trunk': 70,
-    'primary': 50,
-    'secondary': 40,
-    'tertiary': 40,
-    'unclassified': 40,
-    'residential': 30
+    'motorway': 85,         # 93
+    'motorway_link': 70,    # 75
+    'trunk': 65,            # 67
+    'trunk_link': 60,       # 66
+    'primary': 55,          # 60
+    'primary_link': 50,     # 65
+    'secondary': 45,        # 57
+    'secondary_link': 40,   # 60
+    'tertiary': 40,         # 43
+    'residential': 30,      # 33
+    'unclassified': 30      # 40
     }
 
 
 def init():
     if not UPDATE_GRAPH:
-        return ox.load_graphml('data/osm/simplified.graphml', edge_dtypes={"oneway": str})
+        G = ox.load_graphml('data/osm/simplified.graphml', edge_dtypes={"oneway": str}, node_dtypes={"osmid": float})
+        return G
 
     # initialize the reader
     osm = OSM("data/osm/oslo_akershus_highways.osm.pbf")
@@ -48,6 +54,10 @@ def init():
 
 
 def assign_travel_time(G):
+    # remove osm speed limits
+    for _, _, d in G.edges(data=True):
+        if d["highway"] in HIGHWAY_SPEEDS.keys() and "maxspeed" in d:
+            d.pop("maxspeed")
     # assign speeds to edges missing data. Impute if highway type is not in dict.
     G = ox.add_edge_speeds(G, HIGHWAY_SPEEDS)
     # calculate and add travel time to edges
@@ -56,12 +66,23 @@ def assign_travel_time(G):
 
 
 def simplify_graph(G):
+    print("Number of nodes and edges", len(G.nodes), len(G.edges))
+
     # remove geometry attribute from edges as simplify will create it
     for _, _, d in G.edges(data=True):
         d.pop('geometry', None)
+
     # remove unnecessary nodes
     G = ox.simplify_graph(G, STRICT_SIMPLIFYING, REMOVE_RINGS)
-    return G
+    print("Number of nodes and edges after simplifying", len(G.nodes), len(G.edges))
+
+    # get a GeoSeries of consolidated intersections
+    G_proj = ox.project_graph(G)
+    # consolidate intersections and rebuild graph topology
+    G2 = ox.consolidate_intersections(G_proj, rebuild_graph=True, tolerance=15, dead_ends=False)
+    print("Number of nodes and edges after consolidating", len(G2.nodes), len(G2.edges))
+
+    return ox.project_graph(G2, to_crs='epsg:4326')
 
 
 def load_coordinates():
@@ -107,7 +128,7 @@ def get_closest_grids(G, grids):
                 minDist = dist
                 closest = grid_id
         # save grid id
-        closest_grids[node] = closest
+        closest_grids[str(float(node))] = closest
 
     # saves to file
     with open(f'data/nodes_closest_grids.json', 'w') as f:
@@ -115,21 +136,21 @@ def get_closest_grids(G, grids):
     return closest_grids
 
 
-def get_route_info(G, ssb_ids, route, update_period, closest_grids):
+def get_route_info(G, ssb_ids, route, closest_grids):
     travel_time = 0
     section_time = 0
     route_grids = []
     # iterate edges in route
     for u, v in zip(route[:-1], route[1:]):
-        edge_time = G.edges[(u, v, 0)]['travel_time']
+        edge = G.edges[(u, v, 0)]
+        edge_time = edge["travel_time"]
         travel_time += edge_time
         section_time += edge_time
         # save position every x min along route
-        if section_time > update_period*60:
-            section_time = 0
-            closest = closest_grids[u]
-            if closest:
-                route_grids.append(ssb_ids[closest])
+        while section_time >= UPDATE_PERIOD*60:
+            closest_grid = ssb_ids[closest_grids[str(float(v))]]
+            route_grids.append(closest_grid)
+            section_time -= UPDATE_PERIOD*60
     
     travel_time = round(travel_time)
     return travel_time, route_grids
@@ -166,8 +187,10 @@ def shortest_paths(G, ids, from_ids, ssb_ids, nearest_nodes, closest_grids, conn
             # find shortest path (by travel time)
             route = nx.shortest_path(G, source_node, target_node, weight="travel_time")
 
+            #ox.plot_graph_route(G, route, route_linewidth=6, node_size=5, bgcolor='k')
+
             # get travel time and route grids
-            travel_time, route_grids = get_route_info(G, ssb_ids, route, UPDATE_LOCATION_PERIOD, closest_grids)
+            travel_time, route_grids = get_route_info(G, ssb_ids, route, closest_grids)
 
             # remove destination from route if it was added
             if ssb_id2 in route_grids:
@@ -183,14 +206,14 @@ def main():
     G = init()
 
     if UPDATE_GRAPH:
-        print("Assigning speed and travel times for edges...")
-        G = assign_travel_time(G)
-
         print("Simplifying graph...")
         G = simplify_graph(G)
 
+        print("Assigning speed and travel times for edges...")
+        G = assign_travel_time(G)
+
         print("Saving graph...")
-        ox.save_graphml(G, 'data/osm/simplified.graphml')
+        ox.save_graphml(G, 'data/osm/simplified.graphml', gephi=False, encoding='utf-8')
 
     if PLOT_GRAPH:
         print("Printing graph...")
@@ -222,7 +245,8 @@ def main():
     for process in processes:
         process.start()
 
-    od = {}
+    od = {"update_period_minutes": UPDATE_PERIOD}
+
     if FILENAME_OLD:
         print("Loading od path matrix")
         with open(f'data/{FILENAME_OLD}', 'r') as r:
