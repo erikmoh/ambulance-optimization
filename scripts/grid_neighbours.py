394d9101bc7e3c135993672358e9f44ef4363b35
Erik Mohn <erikmoh@stud.ntnu.no>
2023-02-19 22:15:46 +0100

    added basestation and hospital in neighbours

--- scripts/grid_neighbours.py
+++ scripts/grid_neighbours.py
@@ -10,6 +10,19 @@ MAX_DISTANCE = 10 # min
 
 # get grid coordinates
 grids = pd.read_csv("data/grid_centroids.csv")
+grids["type"] = "grid"
+# get base station coordinates
+base_stations = pd.read_csv("data/base_stations.csv")
+base_stations = base_stations[["easting", "northing", "latitude", "longitude"]]
+base_stations.columns = ("xcoor", "ycoor", "lat", "long")
+base_stations["type"] = "base_station"
+# get base station coordinates
+hospitals = pd.read_csv("data/hospitals.csv")
+hospitals = hospitals[["xcoor", "ycoor", "lat", "long"]]
+hospitals.columns = ("xcoor", "ycoor", "lat", "long")
+hospitals["type"] = "hospital"
+# combine coordinates
+all = pd.concat([grids, base_stations, hospitals], ignore_index=True)
 
 print("Loading od_paths")
 od_paths = {}
@@ -18,10 +31,12 @@ with open(f'data/od_paths.json', 'r') as r:
 
 neighbours = {}
 
-for x1, y1, lat1, lon1 in tqdm(grids.values, desc="Finding grid neighbours"):
+for x1, y1, lat1, lon1, type in tqdm(all.values, desc="Finding grid neighbours"):
     start = str(utm_to_ssb_grid_id(x1, y1))
+    if type != "grid":
+        start = f"_{x1:.0f}_{y1:.0f}"
     neighbours[start] = {}
-    for x2, y2, lat2, lon2 in grids.values:
+    for x2, y2, lat2, lon2, _ in grids.values:
         end = str(utm_to_ssb_grid_id(x2, y2))
         distance = round(od_paths[start][end]["travel_time"]/60, 2)
         if distance < MAX_DISTANCE:
